322c5f497a41082754f7fdb1d748039bcbb93945
Sean Gibat <sgibat@gmail.com>
2014-10-09 23:23:34 -0400

    ah what the hell

--- src/com/seangibat/hpnmobile/MainActivity.java
+++ src/com/seangibat/hpnmobile/MainActivity.java
@@ -4,32 +4,26 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 
 
 public class MainActivity extends Activity {
-    
+	protected FrameLayout webViewPlaceholder;
     private WebView mWebView;
     private SwipeRefreshLayout refresher;
     
     @SuppressLint("SetJavaScriptEnabled") @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
-        refresher = (SwipeRefreshLayout) findViewById(R.id.container);
-        
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-        webSettings.setDisplayZoomControls(false);
+    	super.onCreate(Bundle savedInstanceState);
+    	setContentView(R.layout.state_preserving_impl);
 
-        mWebView.loadUrl("http://forums.hipinion.com/");
-        mWebView.setWebViewClient(new MyAppWebViewClient(MainActivity.this, refresher));
+        initUI();
         
+        refresher = (SwipeRefreshLayout) findViewById(R.id.container);
         refresher.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
@@ -40,6 +34,24 @@ public class MainActivity extends Activity {
                 R.color.color2,
                 R.color.color3,
                 R.color.color4);
+    }
+    
+    private void initUI(){
+    	webViewPlaceholder = (FrameLayout) findViewById(R.id.activity_main_webview);
+    	
+    	if (mWebView == null){
+    		mWebView = new WebView(this);
+    		mWebView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+            WebSettings webSettings = mWebView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webSettings.setSupportZoom(true);
+            webSettings.setBuiltInZoomControls(true);
+            webSettings.setDisplayZoomControls(false);
+            mWebView.loadUrl("http://forums.hipinion.com/");
+            mWebView.setWebViewClient(new MyAppWebViewClient(MainActivity.this, refresher));
+    	}
+    	
+    	webViewPlaceholder.addView(mWebView);    	
     }
     
     private void initiateRefresh() {
